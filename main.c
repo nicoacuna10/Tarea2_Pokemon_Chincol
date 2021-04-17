b03c60424028b0bf1c0d7eb2aac7289e9e1429de
CarlosR759 <mipop@pop-os.localdomain>
2021-04-17 13:56:36 -0400

    Menu completado!

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "hashmap.h"
+//#include "hashmap.h"
 
 typedef struct{
 	int id;
@@ -21,7 +21,8 @@ typedef struct{
 	char* region;
 }Pokedex;
 
-/*void menu(int *opcion);
+void menu(int *opcion);
+/*
 void *importarExportarPokemonDesdeUnArchivo(char *nombre_archivo);
 void pokemonAtrapado(char* nombre, char** tipos, int PC, int PS, char* sexo, char* evolucionPrevia, char* evolucionPosterior, int numeroPokedex, char* region);
 void evolucionarPokemon(int id);
@@ -34,6 +35,28 @@ void liberarPokemon(int id);
 void mostrarPokemonPorRegion(char* region);*/
 
 int main(void){
+	int opcionElegida;
+
+
+
+	do{
+		menu(&opcionElegida);
+		if(opcionElegida == 0) break;
+		///if(opcionElegida == 1) importarExportarPokemonDesdeUnArchivo();
+		//if(opcionElegida == 2) pokemonAtrapado();
+		//if(opcionElegida == 3) evolucionarPokemon();
+		//if(opcionElegida == 4) buscarMisPokemonPorTipo();
+		//if(opcionElegida == 5) buscarMisPokemonPorNombre();
+		//if(opcionElegida == 6) buscarPorNombreEnPokedex();
+		//if(opcionElegida == 7) mostrarTodosLosPokemonDeLaPodex();
+		//if(opcionElegida == 8) mostrarPokemonPorMayorPc();
+		//if(opcionElegida == 9) liberarPokemon();
+		//if(opcionElegida == 10) mostrarPokemonPorRegion();
+	}while(menu != 0);
+
+
+
+
 
 	return 0;
 }
--- menu.c
+++ menu.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 void menu(int *opcion){
+ 	char entrada[5000];
+	bool esDiez = false;
+
+	do{
+ 		memset(entrada, '\0', 5000);	
+
+		printf(" ---------------------------------------------------- \n");
+		printf("|                 MENU DE APLICACION                 |\n");
+		printf(" ---------------------------------------------------- \n");
+		printf("| opcion 1: Importar desde archivo                   |\n");
+		printf("| opcion 2: Ingresar pokemon atrapado                |\n");
+		printf("| opcion 3: Buscar siguiente evoluacion              |\n");
+		printf("| opcion 4: Buscar  mis pokemons por tipo            |\n");
+		printf("| opcion 5: Buscar mis pokemons por nombre           |\n");
+		printf("| opcion 6: Buscar nombre en pokedex                 |\n");
+		printf("| opcion 7: Mostrar todos los pokemons de la pokedex |\n");
+		printf("| opcion 8: Mostrar mis pokemons ordenados por PC    |\n");
+		printf("| opcion 9: Liberar pokemon                          |\n");
+		printf("| opcion 10: Mostrar pokemon por region              |\n");
+		printf("| opcion 0: Salir del programa                       |\n");
+		printf(" ---------------------------------------------------- \n");
+
+		printf("\nIngrese opcion: ");
+		scanf("%[^\n]s", entrada);
+		getchar();
+
+		//Se corrobora si entrada no es válida o se ingreso 10//
+		if(entrada[1] != '\0'){
+			if(entrada[1] != '0'){
+			printf("¡Entrada no valida!\n\n");
+			}
+			if(entrada[0] == 1 && entrada[1] == 0) 
+				esDiez = true;
+		}	
+
+	}while( ( (  entrada[0] < '0') || ( entrada[0] > '9') )  );
+
+	//Se ingresan datos correspondientes para main.c//
+	if(esDiez == true)
+		(*opcion) = 10;
+	else
+		(*opcion) = entrada[0] - 48;
+
 	return;
 }
